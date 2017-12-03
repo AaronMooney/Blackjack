2daa35dac875fd1d64f00afb821cc61152931fc1
Aaron Mooney <aarongmooney@gmail.com>
2017-12-03 08:06:29 -0800

    a

---
+++ Blackjack/Dealer.swift
@@ -0,0 +1,26 @@
+//
+//  Dealer.swift
+//  Blackjack
+//
+//  Created by Aaron Mooney on 03/12/2017.
+//  Copyright © 2017 Aaron Mooney. All rights reserved.
+//
+
+import Foundation
+
+class Dealer:BasePlayer{
+    private var card1 = Card(suit: "back", value: 0)
+    
+    override init(hand: Hand){
+        super.init(hand: hand)
+    }
+    
+    func setCard1(card: Card){
+        card1 = card
+        
+    }
+    
+    func getCard1()->Card{
+        return card1
+    }
+}
